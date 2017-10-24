64dc160cc43c0b38a273a14c1ac84c5d19fcc129
Sana Joshi <sanajoshi@Sana-MacBook-1.local>
2017-10-24 17:19:47 +1100

    adding pay button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024022834) do
+ActiveRecord::Schema.define(version: 20171024055301) do
 
   create_table "orders", force: :cascade do |t|
     t.float "total"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20171024022834) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stripe_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
